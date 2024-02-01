e4d72e145e65bb9c143c24513122995935f08079
BeardyMike <42244545+BeardyMike@users.noreply.github.com>
2024-02-01 14:12:40 +0000

    Update AjaxController.php

--- application/Controller/AjaxController.php
+++ application/Controller/AjaxController.php
@@ -1,48 +1,48 @@
 <?php
-/**
- * Class HomeController
+/*
+ * Class AjaxController
  *
  * Please note:
  * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
  * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
  *
- */
+*/
 
 namespace MINIM1\Controller;
 
 class AjaxController
 {
-    /**
+    /*
      * PAGE: create
      * This method handles the ajax calls for create functions and the returns a message or page output
-     */
+    */
     public function create($section="", $action="", $var1="", $var2="", $var3="")
     {
         // Do create stuff
     }
 
-    /**
+    /*
      * PAGE: create
      * This method handles the ajax calls for read functions and the returns a message or page output
-     */
+    */
     public function read($section="", $action="", $var1="", $var2="", $var3="")
     {
         // Do read stuff
     }
 
-    /**
+    /*
      * PAGE: read
      * This method handles the ajax calls for update functions and the returns a message or page output
-     */
+    */
     public function update($section="", $action="", $var1="", $var2="", $var3="")
     {
        // Do update stuff
     }
 
-    /**
+    /*
      * PAGE: delete
      * This method handles the ajax calls for delete functions and the returns a message or page output
-     */
+    */
     public function delete($section="", $action="", $var1="", $var2="", $var3="")
     {
         // Do delete stuff
