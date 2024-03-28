575671b3a00511d704a06affe5c12b26ba88a306
BeardyMike <42244545+BeardyMike@users.noreply.github.com>
2024-03-28 13:26:15 +0000

    FileNameCapitalisationFix - part 1
    Removing ALL files, and then reuploading them with the correct name.

--- application/view/error/index.php
+++
@@ -1,3 +0,0 @@
-<div class="container">
-    <p>This is the Error-page. Will be shown when a page (= controller / method) does not exist.</p>
-</div>
--- application/view/home/index.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <h1>Home</h1>
-    <h2>You are in the View: application/view/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
-</div>
-<div class="container">
-    <div id="ajaxresponse"></div>
-</div>
